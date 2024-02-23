f0640e7f7009ce53443bcaf402fc93c5c073b82b
TheTest <mattmandery@gmail.com>
2024-02-22 19:50:20 -0500

    Added Service and Service.Tests project.
    Made working ExcelImport Service.

---
+++ FerrariAwardGenerator.Service.Tests/ExcelImport/ExcelImportServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using FerrariAwardGenerator.Service.ExcelImport.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace FerrariAwardGenerator.Service.Tests.ExcelImport
+{
+    [TestClass]
+    public class ExcelImportServiceTests
+    {
+        ExcelImportService _excelImportService;
+
+        public ExcelImportServiceTests()
+        {
+            _excelImportService = new ExcelImportService();
+        }
+
+        [TestMethod]
+        public void ExcelImportServiceTest_ReadExcelFile()
+        {
+            string filePath = "C:/Users/mattm/Downloads/FCA Class results.xls";
+
+            var sut = _excelImportService.ReadExcelFile(filePath);
+
+            Assert.IsNotNull(sut);
+        }
+    }
+}
+
---
+++ FerrariAwardGenerator.Service/ExcelImport/Services/ExcelImportService.cs
@@ -0,0 +1,22 @@
+﻿using FerrariAwardGenerator.Service.ExcelImport.Models;
+using Ganss.Excel;
+using System.Data;
+
+namespace FerrariAwardGenerator.Service.ExcelImport.Services
+{
+    public class ExcelImportService
+    {
+
+        public ExcelImportService()
+        {
+
+        }
+
+        public List<ExcelImportModel> ReadExcelFile(string filePath)
+        {
+            var excelMapper = new ExcelMapper(filePath);
+            var books = excelMapper.Fetch<ExcelImportModel>().ToList();
+            return books;
+        }
+    }
+}
